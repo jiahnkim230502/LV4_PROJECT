50fd0867e9c54e354eeaf400de77913bc5c77667
jiahnkim230502 <locker9417@gmail.com>
2023-07-07 17:35:25 +0900

    댓글삭제 기능 구현

--- routes/comment.js
+++ routes/comment.js
@@ -12,7 +12,7 @@ const { Op } = require("sequelize");
 router.get("/posts/:postId/comments", async (req, res) => {
   // 2. 조회하는 게시글에 작성된 모든 댓글을 목록 형식으로 볼 수 있도록 하기
   const comments = await Comments.findAll({
-    attributes: ["postId", "content", "createdAt", "updatedAt"],
+    attributes: ["postId", "commentId", "content", "createdAt", "updatedAt"],
     // 3. 작성 날짜 기준으로 내림차순 정렬하기
     order: [["createdAt", "DESC"]],
   });
@@ -93,7 +93,36 @@ router.put("/posts/:postId/comments/:commentId", authMiddleware, async (req, res
   );
 
   return res.status(200).json({ message: "게시글이 수정되었습니다." });
-}
-);
+});
+
+// 댓글 삭제 API
+// 10. 로그인 토큰을 검사하여, 해당 사용자가 작성한 댓글만 삭제 가능
+router.delete("/posts/:postId/comments/:commentId", authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { commentId } = req.params;
+
+  const CommentId = await Comments.findOne({
+    where: { commentId },
+  });
+
+  if (!CommentId) {
+    return res.status(404).json({
+      message: "게시글이 존재하지 않습니다.",
+    });
+  } else if (CommentId.UserId !== userId) {
+    return res.status(403).json({
+      message: "게시글 삭제 권한이 있는 사용자가 아닙니다.",
+    });
+  }
+
+  // 11. 원하는 댓글을 삭제하기
+  await Comments.destroy({
+    where: {
+      [Op.and]: [{ userId }, { commentId }],
+    },
+  });
+
+  return res.status(200).json({ message: "게시글이 삭제되었습니다." });
+});
 
 module.exports = router;
